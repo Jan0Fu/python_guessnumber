254e72fc7d8c343ce3340d71b90ae456e55f2972
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-22 16:26:25 +0100

    Main part of program is finished

--- guessnumber.py
+++ guessnumber.py
@@ -1,19 +1,57 @@
 import random
 
-user_number = int(input("Guess a number from 1 - 10: "))
-if user_number > 10 or user_number < 0:
-    print("Wrong input!")
 random_number = random.randrange(1, 11)
+user_number = 0
 
-if user_number == random_number :
-    print("Jackpot!")
+def new_input() :
+    user_number = int(input("You have 3 attempts, guess a number from 1 - 10: "))
+    if user_number > 10 or user_number < 0:
+        print("Wrong input!")
+    if type(user_number) == str :
+        print("Not a number")
+
+new_input()
+
+def checker() :
+    if  user_number < random_number :
+        print(("Wrong! Try bigger number: "))
+    elif user_number > random_number :
+        print("Wrong! Try lesser number: ")
+    else :
+        print("Jackpot!")
+
+checker()
+
 
 if user_number > random_number :
     print("Wrong!")
-    user_number = input("Try lesser number: ")
+    user_number = int(input("Try lesser number: "))
+    if user_number == random_number :
+        print("Jackpot!")
+    if user_number > random_number :
+        print("Wrong!")
+        user_number = int(input("Try lesser number: "))
+        if user_number == random_number :
+            print("Jackpot!")
+    if user_number < random_number :
+        print("Wrong!")
+        user_number = int(input("Try bigger number: "))
+        if user_number == random_number :
+            print("Jackpot!")
+
 
 if user_number < random_number :
     print("Wrong!")
-    user_number = input("Try bigger number: ")
+    user_number = int(input("Try bigger number: "))
     if user_number == random_number :
         print("Jackpot!")
+    if user_number > random_number :
+        print("Wrong!")
+        user_number = int(input("Try lesser number: "))
+        if user_number == random_number :
+            print("Jackpot!")
+    if user_number < random_number :
+        print("Wrong!")
+        user_number = int(input("Try bigger number: "))
+        if user_number == random_number :
+            print("Jackpot!")
