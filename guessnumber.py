c847e6b169e96cf03a66963365ae9cda88c4fe89
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-22 16:23:16 +0100

    Created random function for project

--- guessnumber.py
+++ guessnumber.py
@@ -1,6 +1,19 @@
 import random
 
-user_number = int(input("Pick a number from 1 - 100: "))
-if user_number > 100 or user_number < 0:
-    print("wrong input")
-random_number = random.randrange(1, 101)
+user_number = int(input("Guess a number from 1 - 10: "))
+if user_number > 10 or user_number < 0:
+    print("Wrong input!")
+random_number = random.randrange(1, 11)
+
+if user_number == random_number :
+    print("Jackpot!")
+
+if user_number > random_number :
+    print("Wrong!")
+    user_number = input("Try lesser number: ")
+
+if user_number < random_number :
+    print("Wrong!")
+    user_number = input("Try bigger number: ")
+    if user_number == random_number :
+        print("Jackpot!")
