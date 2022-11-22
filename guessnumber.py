9ae2157d09978148e605f4b4f98a6f7ff2b8ab8d
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-22 19:51:37 +0100

    Added attempts feature, fixed bud

--- guessnumber.py
+++ guessnumber.py
@@ -2,16 +2,23 @@ import random
 
 random_number = random.randrange(1, 101)
 user_number = 0
+attempts = 8
 
 while user_number != random_number :
     user_number = int(input("Guess a number from 1 - 100: "))
-    if type(user_number) == str :
-        print("Not a number!")
+    if attempts == 1 :
+        print("Game over!")
+        break
     elif user_number > 100 or user_number < 1:
         print("Wrong input!")
     elif user_number > random_number :
-        print("Try lesser number")
+        attempts = attempts - 1
+        print("Try lesser number, attempts: ", attempts)
+    elif user_number < random_number :
+        attempts = attempts - 1
+        print("Try bigger number, attempts: ", attempts)
     else :
-        print("Try bigger number")
+        user_number == random_number
+        print("Jackpot! Its number:",random_number, "You won!")
 
-print("You won! its", random_number)
+print("Number was", random_number)
